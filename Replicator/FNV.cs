384087b4d66a0899ead353b075a840885c8513ce
Sergey Seletsky <SeletskySergey@live.com>
2016-12-12 16:01:22 +0200

    added replicator

---
+++ Replicator/FNV.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Replicator
+{
+    public static class FNV
+    {
+        private const ulong FNV64Offset = 14695981039346656037;
+
+        private const ulong FNV64Prime = 1099511628211;
+
+        private const ulong FNV64Space = ulong.MaxValue - FNV64Prime;
+
+        public static ulong HashFNV1A(this string key)
+        {
+            var bytes = Encoding.Default.GetBytes(key.ToUpperInvariant());
+            var hash = FNV64Offset;
+            foreach (var @byte in bytes)
+            {
+                hash ^= @byte;
+                hash *= FNV64Prime;
+            }
+            return hash;
+        }
+
+        public class Partition
+        {
+            public int Id;
+            public Dictionary<ulong, string> Items = new Dictionary<ulong, string>();
+            public Partition(ulong begin, ulong end)
+            {
+                Begin = begin;
+                End = end;
+            }
+
+            public ulong Begin;
+            public ulong End;
+        }
+
+        public static List<Partition> AllocatePartitions(ushort count)
+        {
+            var part = FNV64Space / count;
+            var shift = FNV64Space % count;
+            var partitions = new List<Partition>();
+
+            var index = 1;
+            for (var begin = FNV64Prime; begin < ulong.MaxValue - shift; begin += part)
+            {
+                index++;
+                if (begin + part >= ulong.MaxValue - shift)
+                {
+                    part += shift;
+                }
+                partitions.Add(new Partition(begin, begin + part) { Id = index });
+            }
+            return partitions;
+        }
+    }
+}
---
+++ Replicator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replicator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = File.ReadAllLines("words.txt").OrderBy(f => f).ToList();
+
+            Console.WriteLine($"Loaded {arr.Count} items");
+
+            var sw = Stopwatch.StartNew();
+            var fnvs = arr.ToDictionary(f => f.HashFNV1A(), f => f);
+            sw.Stop();
+            Console.WriteLine($"Elapsed ms {sw.ElapsedMilliseconds}");
+
+            for (ushort i = 2; i <= 8; i++)
+            {
+                Console.WriteLine($"\n\nPartitions {i} used");
+                var spaces = FNV.AllocatePartitions(i);
+
+                foreach (var fnv in fnvs)
+                {
+                    foreach (var space in spaces)
+                    {
+                        if (space.Begin <= fnv.Key && fnv.Key <= space.End)
+                        {
+                            space.Items.Add(fnv.Key, fnv.Value);
+                        }
+                    }
+                }
+
+                var totalFnvs = 0;
+                foreach (var space in spaces)
+                {
+                    totalFnvs += space.Items.Count;
+                    Console.WriteLine($"Space: begin({space.Begin}):end({space.End}) contains {space.Items.Count} items");
+                }
+                Console.WriteLine($"Total: {totalFnvs} source total: {fnvs.Count}");
+            }
+        }
+    }
+}
