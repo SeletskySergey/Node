b44f30034a337a8c330478f516135d6a1478f1fa
Sergey Seletsky <SeletskySergey@live.com>
2020-09-16 10:52:40 +0300

    fixed

--- Replicator/Program.cs
+++ Replicator/Program.cs
@@ -1,19 +1,90 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Replicator
 {
     class Program
     {
+        public static int GetDigits(long n)
+        {
+            if (n < 10L) return 1;
+            if (n < 100L) return 2;
+            if (n < 1000L) return 3;
+            if (n < 10000L) return 4;
+            if (n < 100000L) return 5;
+            if (n < 1000000L) return 6;
+            if (n < 10000000L) return 7;
+            if (n < 100000000L) return 8;
+            if (n < 1000000000L) return 9;
+            if (n < 10000000000L) return 10;
+            if (n < 100000000000L) return 11;
+            if (n < 1000000000000L) return 12;
+            if (n < 10000000000000L) return 13;
+            if (n < 100000000000000L) return 14;
+            if (n < 1000000000000000L) return 15;
+            if (n < 10000000000000000L) return 16;
+            if (n < 100000000000000000L) return 17;
+            if (n < 1000000000000000000L) return 18;
+            return 19;
+        }
+
         static void Main()
         {
+            var hash = ulong.MaxValue;
+            var sw = Stopwatch.StartNew();
+
+            for (var i = 0; i <= 1000000; i++)
+            {
+                var code16 = Unified.NewHex16(hash);
+                var dc16 = Unified.Decode16(code16);
+            }
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+            sw.Restart();
+            for (var i = 0; i <= 1000000; i++)
+            {
+                var code = Unified.NewHex32(hash);
+                var dc = Unified.Decode32(code);
+            }
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+            sw.Restart();
+            for (var i = 0; i <= 1000000; i++)
+            {
+                var code64 = Unified.NewHex64(hash);
+                var dc64 = Unified.Decode64(code64);
+            }
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+
+
+
+            var digits = 0;
+            var set = new HashSet<ulong>();
+
+            while (true)
+            {
+                if (!set.Add(hash))
+                {
+                    Console.WriteLine($"Collision on: {set.Count}");
+                }
+
+                var currentDigits = GetDigits(set.Count);
+                if (digits != currentDigits)
+                {
+                    digits = currentDigits;
+                    Console.WriteLine($"Digits: {digits}");
+                }
+            }
+
             var arr = File.ReadAllLines("words.txt").OrderBy(f => f).ToList();
 
             Console.WriteLine($"Loaded {arr.Count} items");
 
-            var sw = Stopwatch.StartNew();
             var fnvs = arr.ToDictionary(f => f.HashFNV1A(), f => f);
             sw.Stop();
             Console.WriteLine($"Elapsed ms {sw.ElapsedMilliseconds}");
--- Replicator/Unified.cs
+++ Replicator/Unified.cs
@@ -1,132 +1,128 @@
 using System;
-using System.Text;
+
+/// <summary>
+/// Unified unique codes generator designed by Sergey Seletsky
+/// </summary>
+public static class Unified
+{
+    // select shift for (x16 >> 4 == 1) dimensions
+    private const int x16Shift = 4;
+
+    // select shift for (x32 >> 5 == 1) dimensions
+    private const int x32Shift = 5;
+
+    // select shift for (x64 >> 6 == 1) dimensions
+    private const int x64Shift = 6;
 
     /// <summary>
-    /// Unified unique codes generator designed by Sergey Seletsky
+    /// FNV x64 Prime https://en.wikipedia.org/wiki/Prime_number
     /// </summary>
-    public static class Unified
+    private const ulong prime = 1099511628211U;
+
+    /// <summary>
+    /// FNV x64 Offset basis
+    /// </summary>
+    private const ulong offset = 14695981039346656037U;
+
+    // Set of symbols used for numeric dimensions transcoding
+    private static char[] symbols = new char[64]
     {
-        // FNV x64 Prime
-        private const ulong prime = 14695981039346656037U;
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+            'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+            'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '$', '@'
+    };
 
-        // FNV x64 Offset
-        private const ulong offset = 1099511628211U;
-
-        // Set of symbols used for numeric dimensions transcoding
-        private static char[] symbols = new char[64]
-        {
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '?', '@'
-        };
-
-        // Generate x64 FNV hash based on random GUID
-        public static ulong NewHash()
-        {
-            return NewHash(Guid.NewGuid().ToByteArray());
-        }
-
-        // Generate x64 FNV hash based on data bytes
-        public static ulong NewHash(byte[] bytes)
-        {
-            var hash = prime; // fnv prime
-
-            foreach (var @byte in bytes)
-            {
-                hash ^= @byte;
-                hash *= offset; // fnv offset
-            }
-
-            return hash;
-        }
-
-        // Generate random x32 hex
-        public static String NewHex()
-        {
-            return NewHex(NewHash());
-        }
-
-        // Generate x32 hex from number
-        public static String NewHex16(ulong n)
-        {
-            var len = 16;
-            char[] ch = new char[len--];
-            for (var i = len; i >= 0; i--)
-            {
-                var inx = (byte)((uint)(n >> 4 * i) & 15);
-                ch[len - i] = symbols[inx];
-            }
-            return new String(ch);
-        }
-
-        // Generate x32 hex from number
-        public static String NewHex(ulong n)
-        {
-            var len = 13;
-            char[] ch = new char[len--];
-            for (var i = len; i >= 0; i--)
-            {
-                var inx = (byte)((uint)(n >> 5 * i) & 31);
-                ch[len - i] = symbols[inx];
-            }
-            return new String(ch);
-        }
-
-        // Generate x64 hex from number
-        public static String NewHex64(ulong n)
-        {
-            var len = 11;
-            char[] ch = new char[len--];
-            for (var i = len; i >= 0; i--)
-            {
-                var inx = (byte)((uint)(n >> 6 * i) & 63);
-                ch[len - i] = symbols[inx];
-            }
-            return new String(ch);
-        }
-
-        // Decode x16 hex to number
-        public static ulong Decode16(string code)
-        {
-            var shift = 4; // select shift for x64 dimensions if lower case detected
-            ulong hash = 0;
-            for (int i = 0; i < code.Length; i++)
-            {
-                var index = (ulong)Array.IndexOf(symbols, code[i]);
-                var nuim = (index << ((code.Length - 1 - i) * shift)); // convert dimension to number and add 
-                hash += nuim;
-            }
-
-            return hash;
-        }
-
-        // Decode x32 hex to number
-        public static ulong Decode(string code)
-        {
-            var shift = 5; // select shift for x64 dimensions if lower case detected
-            ulong hash = 0;
-            for (int i = 0; i < code.Length; i++)
-            {
-                var index = (ulong)Array.IndexOf(symbols, code[i]);
-                var nuim = (index << ((code.Length - 1 - i) * shift)); // convert dimension to number and add 
-                hash += nuim;
-            }
-
-            return hash;
-        }
-
-        // Decode x64 hex to number
-        public static ulong Decode64(string code)
-        {
-            var shift = 6; // select shift for x64 dimensions if lower case detected
-            ulong hash = 0;
-            for (int i = 0; i < code.Length; i++)
-            {
-                var index = (ulong)Array.IndexOf(symbols, code[i]);
-                var nuim = (index << ((code.Length - 1 - i) * shift)); // convert dimension to number and add 
-                hash += nuim;
-            }
-
-            return hash;
-        }
+    // Generate x64 FNV hash based on random GUID
+    public static ulong NewHash()
+    {
+        return NewHash(Guid.NewGuid().ToByteArray());
     }
+
+    // Generate x64 FNV hash based on data bytes
+    public static ulong NewHash(byte[] bytes)
+    {
+        var hash = offset; // fnv offset basis
+
+        foreach (var @byte in bytes)
+        {
+            hash ^= @byte;
+            hash *= prime; // fnv prime
+        }
+
+        return hash;
+    }
+
+    // Generate random x32 hex
+    public static string NewHex()
+    {
+        return NewHex32(NewHash());
+    }
+
+    // Generate x32 hex from number
+    public static string NewHex16(ulong hash)
+    {
+        return NewHex(hash, x16Shift, 16);
+    }
+
+    // Generate x32 hex from number
+    public static string NewHex32(ulong hash)
+    {
+        return NewHex(hash, x32Shift, 13);
+    }
+
+    // Generate x64 hex from number
+    public static string NewHex64(ulong hash)
+    {
+        return NewHex(hash, x64Shift, 11);
+    }
+
+    private static string NewHex(ulong hash, int shift, int length)
+    {
+        char[] hex = new char[length--];
+        for (var grade = length; grade >= 0; grade--)
+        {
+            // index = >> hash >> slice(shift * grade) & dimension(1<<shift) - 1
+            var index = (byte)((uint)(hash >> shift * grade) & (1 << shift) - 1);
+            hex[length - grade] = symbols[index];
+        }
+        return new string(hex);
+    }
+
+    // Decode x16 hex to number
+    public static ulong Decode16(string hex)
+    {
+        return Decode(hex, x16Shift);
+    }
+
+    // Decode x32 hex to number
+    public static ulong Decode32(string hex)
+    {
+        return Decode(hex, x32Shift);
+    }
+
+    // Decode x64 hex to number
+    public static ulong Decode64(string hex)
+    {
+        return Decode(hex, x64Shift);
+    }
+
+    /// <summary>
+    /// Decode HEX to Number
+    /// </summary>
+    /// <param name="hex">String HEX</param>
+    /// <param name="shift">Shift of (dimension >> 1 ... == 1)</param>
+    /// <returns>Unsigned x64 integer</returns>
+    private static ulong Decode(string hex, int shift)
+    {
+        ulong hash = 0;
+        for (int i = 0; i < hex.Length; i++)
+        {
+            var index = (ulong)Array.IndexOf(symbols, hex[i]);
+            // slice grade and convert to number
+            var grade = index << ((hex.Length - 1 - i) * shift);
+            hash += grade;
+        }
+        return hash;
+    }
+}
